61a84222e92b3eff5bee8a0a5b803e81e0d6cffd
Igor Caetano <igorcaetano010@gmail.com>
2026-09-17 14:39:48 -0300

    arquivo checker finalizado com diagnostico de gateway, ping e dns

--- netmonitor/checker.py
+++ netmonitor/checker.py
@@ -1,21 +1,22 @@
 import subprocess
 import re
+import socket
 
 def get_default_gateway():
-	try:
-		result = subprocess.run(
-			['ip', 'route', 'show', 'default'],
-			capture_output=True,
-			text=True,
-			check=True
-		)	
-		
-		match = re.search(r"default via ([0-9]+(?:\.[0-9]+){3})", result.stdout)
-		if match:
-			return match.group(1)
-		return None
-	except (subprocess.CalledProcessError, FileNotFoundError):
-		return None
+    try:
+        result = subprocess.run(
+            ['ip', 'route', 'show', 'default'],
+            capture_output=True,
+            text=True,
+            check=True
+        )	
+        
+        match = re.search(r"default via ([0-9]+(?:\.[0-9]+){3})", result.stdout)
+        if match:
+            return match.group(1)
+        return None
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return None
 
 def ping_host(host, count=3, timeout=2):
     try:
@@ -33,24 +34,37 @@ def ping_host(host, count=3, timeout=2):
             "packet_loss": packet_loss,
             "avg_rtt": avg_rtt
         }
-    except Exception:
+    except (subprocess.SubprocessError, OSError):
         return {
             "online": False,
             "packet_loss": 100.0,
             "avg_rtt": None 
         }
 
+def check_dns(host="google.com"):
+    try:
+        socket.setdefaulttimeout(3)
+        ip_founded = socket.gethostbyname(host)
+        return {
+            "resolved" : True,
+            "ip" : ip_founded
+        }
+    
+    except (socket.gaierror, socket.timeout):
+        return {
+            "resolved" : False,
+            "ip" : None 
+        } 
+
 if __name__ == "__main__":
     gateway = get_default_gateway()
-    print(f"Gateway Padrão: {gateway}")
+    print(f"Default Gateway: {gateway}")
+    print(" ")
 
     if gateway:
-        resultado = ping_host(gateway)
-        print("Status Gateway:")
-        for valor in resultado:
-            if valor == 'avg_rtt':
-                print(f'{valor}: {resultado[valor]}ms', end=' ')
-            else:
-                print(f'{valor}: {resultado[valor]} ; ', end=' ')
-        print()
-        
+        result_gateway = ping_host(gateway)
+        rtt = f"{result_gateway['avg_rtt']}ms" if result_gateway['avg_rtt'] is not None else "N/A"
+        print(f"Gateway Status: online={result_gateway['online']} ; loss={result_gateway['packet_loss']}% ; avg_rtt={rtt}\n")
+
+    result_dns = check_dns()
+    print(f"DNS Resolution: resolved={result_dns['resolved']} ; ip={result_dns['ip']}") 
